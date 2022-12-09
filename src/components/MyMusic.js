82171ab6cafad594ee9d98001f123ec96cf95c4b
soloplxya <shainesnake@gmail.com>
2022-12-10 00:14:30 +0800

    fix: mobile responsiveness

--- src/components/MyMusic.js
+++ src/components/MyMusic.js
@@ -7,7 +7,7 @@ export default class MyMusic extends Component {
     return (
       <section id="my_music">
          <div className="row">
-            <div className="my_music container">
+            <div className="my_music container"> 
                 <div>
                     <h2>  
                         <TypewriterComponent
@@ -28,12 +28,14 @@ export default class MyMusic extends Component {
                             }}
                             /> 
                         </h2>
-                    <p> I enjoy listening to a wide variety of music! My current favourites are English oldies, especially songs from Frank Sinatra. :D I also really enjoy listening to Japanese retro music from the 1970s.</p>
+                    <p > I enjoy listening to a wide variety of music! My current favourites are English oldies, especially songs from Frank Sinatra. :D I also really enjoy listening to Japanese retro music from the 1970s.</p>
                 </div>
-                <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/7pypFNE6K9Ocd3EYqVVBDM?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/7MtspAwIajxdyCsUsVDYo0?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <div>  
+                    <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/7pypFNE6K9Ocd3EYqVVBDM?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                    <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/7MtspAwIajxdyCsUsVDYo0?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </div>
             </div>
-         </div>
+        </div>
       </section>
     );
   }
